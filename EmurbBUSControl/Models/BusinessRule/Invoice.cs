3899d0b54de56494b2e48472e418c4393e8abee8
Ottoniel Matheus <ottoniel.matheus@gmail.com>
2019-09-26 22:34:36 -0300

    metodos relacionados ao faturamento adicionado

---
+++ EmurbBUSControl/Models/BusinessRule/Invoice.cs
@@ -0,0 +1,15 @@
+﻿using EmurbBUSControl.Models.DataModels.BusinessRule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmurbBUSControl.Models.BusinessRule
+{
+    public class Invoice
+    {
+        public Company Debtor { get; set; }
+        public decimal TotalCost { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+    }
+}
